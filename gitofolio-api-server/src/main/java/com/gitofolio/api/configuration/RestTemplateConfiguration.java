ee5101c5f68b7dfd43202606aca0d0139b245593
devxb <devxb@naver.com>
2022-01-05 11:55:04 +0000

    add: hikaricp 연결갯수등 설정 추가, RestTemplate 설정 추가

--- gitofolio-api-server/src/main/java/com/gitofolio/api/configuration/RestTemplateConfiguration.java
+++ gitofolio-api-server/src/main/java/com/gitofolio/api/configuration/RestTemplateConfiguration.java
@@ -3,6 +3,10 @@ package com.gitofolio.api.configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 @Configuration
 public class RestTemplateConfiguration{
@@ -10,7 +14,17 @@ public class RestTemplateConfiguration{
 	@Bean
 	public RestTemplate restTemplate(){
 		
-		RestTemplate restTemplate = new RestTemplate();
+		HttpClient httpClient = HttpClients.custom()
+			.setMaxConnTotal(20)
+			.setMaxConnPerRoute(4)
+			.build();
+		
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		
+		httpComponentsClientHttpRequestFactory.setConnectTimeout(5000);
+		httpComponentsClientHttpRequestFactory.setReadTimeout(5000);
+		
+		RestTemplate restTemplate = new RestTemplate(httpComponentsClientHttpRequestFactory);
 		
 		return restTemplate;
 	}
