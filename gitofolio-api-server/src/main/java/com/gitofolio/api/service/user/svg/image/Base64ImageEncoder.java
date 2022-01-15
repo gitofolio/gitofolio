a53e8ef8c5b071f5b6f981b9c62738d119309d0d
devxb <devxb@naver.com>
2022-01-15 14:49:49 +0000

    fix: 유저 프로필 이미지 최적화 퀄리티 우선으로 변경

--- gitofolio-api-server/src/main/java/com/gitofolio/api/service/user/svg/image/Base64ImageEncoder.java
+++ gitofolio-api-server/src/main/java/com/gitofolio/api/service/user/svg/image/Base64ImageEncoder.java
@@ -30,14 +30,18 @@ public class Base64ImageEncoder implements ImageEncoder{
 	}
 	
 	private BufferedImage optimizeImage(BufferedImage bufferedImage){
-		BufferedImage ans = new BufferedImage(45, 45, bufferedImage.getType());
+		BufferedImage ans = new BufferedImage(100, 100, bufferedImage.getType());
 		
 		Graphics2D graphics2D = ans.createGraphics();
-		graphics2D.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
-		graphics2D.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
-		graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+		// graphics2D.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
+		// graphics2D.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+		// graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+		// graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+		graphics2D.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		graphics2D.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 		graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
-		graphics2D.drawImage(bufferedImage, 0, 0, 45, 45, null);
+		graphics2D.drawImage(bufferedImage, 0, 0, 100, 100, null);
 		graphics2D.dispose();
 		
 		return ans;
